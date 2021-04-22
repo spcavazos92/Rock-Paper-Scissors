f49999ee82edeacbef56e0ed469e445d50c093cc
Sergio Cavazos <spaul.cavazos@gmail.com>
2021-04-22 17:47:55 -0500

    terminado juego a 5 ronda solo en consola

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-let computerPlay = () => Math.floor(Math.random()*3);
+let computerPlay = () => {
+    let aleatorio=Math.floor(Math.random()*3);
+    if (aleatorio==1){
+        aleatorio="Piedra";
+    } else if(aleatorio==2){
+        aleatorio="Papel";
+    } else {
+        aleatorio="Tijera";
+    }
+    return aleatorio;
+};
 
-console.log(computerPlay());
+// console.log(computerPlay());
 
 let playerPlay = () => {
-    let respuesta = (prompt("Make your choice (Rock/Paper/Scissors)",)).toUpperCase();
-    if (respuesta==="PAPER" || respuesta==="SCISSORS" || respuesta==="ROCK"){
+    let respuesta = (prompt("Make your choice (Piedra/Papel/Tijera)",)).toUpperCase();
+    if (respuesta==="PIEDRA" || respuesta==="PAPEL" || respuesta==="TIJERA"){
+        respuesta=respuesta.toLowerCase();
+        respuesta=respuesta.charAt(0).toUpperCase()+respuesta.slice(1);
         return respuesta;
     } else {
         alert("Respuesta invalida, ingrese nuevamente");
@@ -13,10 +25,43 @@ let playerPlay = () => {
     }
 };
 
-console.log(playerPlay());
+// console.log(playerPlay());
 
 let ronda = () => {
-    if (playerPlay=="PAPER" && computerPlay==1){
-        
+    let resultado;
+    let explicacion;
+    let jugador=playerPlay();
+    let computadora=computerPlay();
+    let par=jugador+computadora;
+    if (par=="PiedraPiedra"||par=="PapelPapel"||par=="TijeraTijera"){
+        resultado="Empataste";
+        explicacion="empata";
+    } else if (par=="PiedraPapel"||par=="PapelTijera"||par=="TijeraPiedra"){
+        resultado="Perdiste";
+        explicacion="pierde";
+    } else {
+        resultado="Ganaste";
+        explicacion="gana";
     }
+    alert(`${resultado}! ${jugador} ${explicacion} contra ${computadora}`);
+    return resultado;
+}
+
+// console.log(ronda());
+
+
+let juego = () => {
+    let scoreJugador=0;
+    let scoreComputadora=0;
+    for (let i=0; i<5; i++){
+        let partida=ronda();
+        if (partida=="Ganaste"){
+            scoreJugador++;
+        } else if (partida=="Perdiste"){
+            scoreComputadora++;
+        }
+    }
+    alert(`El resultado final es ${scoreJugador} JUGADOR | COM ${scoreComputadora} con ${5-scoreComputadora-scoreJugador} empate(s)`);
 };
+
+juego();
